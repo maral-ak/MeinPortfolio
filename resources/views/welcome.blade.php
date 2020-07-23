ede0b07611120b743f50dccf706001845cca11c4
Maral <maral@MacBook-Pro-di-Apple.local>
2020-07-23 11:57:34 +0200

    modify welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to you :)
                 </div>
 
                 <div class="links">
